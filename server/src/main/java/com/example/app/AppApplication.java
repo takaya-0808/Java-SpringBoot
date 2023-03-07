72c2bc68c7739f2cb49218c6033d5fae32cce3cb
takaya-0808 <takaya1912@gmail.com>
2023-03-07 12:47:07 +0900

    fix

--- server/src/main/java/com/example/app/AppApplication.java
+++ server/src/main/java/com/example/app/AppApplication.java
@@ -1,10 +1,15 @@
 package com.example.app;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.ui.Model;
 
 @SpringBootApplication
 @RestController
@@ -15,9 +20,19 @@ public class AppApplication {
 		return "Hello World";
 	}
 
+	@RequestMapping(value="/index", method=RequestMethod.GET) 
+	public String HelloGetWorld(Model model) {
+		return "index";
+	}
+
 	@RequestMapping("/tests/{param}")
 	private String testParamVariable(@PathVariable String param) {
 		return "return + " + param;
+	}
+
+	@RequestMapping(value="/resource/{id}", method=RequestMethod.GET)
+	private String read(@PathVariable String id) {
+		return "reference: " + id;
 	}
 
 	public static void main(String[] args) {
