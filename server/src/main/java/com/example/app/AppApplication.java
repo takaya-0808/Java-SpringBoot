2f5a7ef825bc53accf6ba8261f47b514760e1826
takaya-0808 <takaya1912@gmail.com>
2023-03-13 23:20:13 +0900

    fix

--- server/src/main/java/com/example/app/AppApplication.java
+++ server/src/main/java/com/example/app/AppApplication.java
@@ -12,28 +12,28 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.ui.Model;
 
 @SpringBootApplication
-@RestController
+// @RestController
 public class AppApplication {
 
-	@RequestMapping("/")
-	public String home() {
-		return "Hello World";
-	}
+	// @RequestMapping("/")
+	// public String home() {
+	// 	return "Hello World";
+	// }
 
-	@RequestMapping(value="/index", method=RequestMethod.GET) 
-	public String HelloGetWorld(Model model) {
-		return "index";
-	}
+	// @RequestMapping(value="/index", method=RequestMethod.GET) 
+	// public String HelloGetWorld(Model model) {
+	// 	return "index";
+	// }
 
-	@RequestMapping("/tests/{param}")
-	private String testParamVariable(@PathVariable String param) {
-		return "return + " + param;
-	}
+	// @RequestMapping("/tests/{param}")
+	// private String testParamVariable(@PathVariable String param) {
+	// 	return "return + " + param;
+	// }
 
-	@RequestMapping(value="/resource/{id}", method=RequestMethod.GET)
-	private String read(@PathVariable String id) {
-		return "reference: " + id;
-	}
+	// @RequestMapping(value="/resource/{id}", method=RequestMethod.GET)
+	// private String read(@PathVariable String id) {
+	// 	return "reference: " + id;
+	// }
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
