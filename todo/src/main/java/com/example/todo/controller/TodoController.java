675167427e54de0fe78b8e387a66e7df80b3584c
takaya-0808 <takaya1912@gmail.com>
2023-03-21 12:25:08 +0900

    add RequestMapping(/todo/api)

--- todo/src/main/java/com/example/todo/controller/TodoController.java
+++ todo/src/main/java/com/example/todo/controller/TodoController.java
@@ -2,3 +2,16 @@ package com.example.todo.controller;
 
 import java.util.*;
 
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/todo/api")
+public class TodoController {
+    @RequestMapping("/")
+    String home() {
+        return "Hello World";
+    }
+
+}
+
