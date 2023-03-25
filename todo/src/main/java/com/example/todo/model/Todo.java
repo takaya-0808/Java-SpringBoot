acc02eb3ec1d793e8cdd01399385910bd26df0ad
takaya-0808 <takaya1912@gmail.com>
2023-03-25 18:12:35 +0900

    add Model function

--- todo/src/main/java/com/example/todo/model/Todo.java
+++ todo/src/main/java/com/example/todo/model/Todo.java
@@ -6,12 +6,50 @@ public class Todo {
 
     private Long id;
     private String title;
-    private String context;
+    private String content;
     private Date createDateTime;
     private Date updateDateTime;
     private Boolean archived;
     private Boolean completed;
 
-    
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return this.content
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getCreateDateTime() {}
+
+    public void setreateDateTime(Date time) {}
+
+    public Date getUpdateDateTime() {}
+
+    public void setUpdateDateTime(Date time) {}
+
+    public Boolean getArchived() {}
+
+    public void setArchived(Boolean archived) {}
+
+    public Boolean getCompleted() {}
+
+    public void setCompleted(Boolean completed) {}
 
 }
