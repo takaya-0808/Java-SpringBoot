53de3503ffc1917a3ef9c5d48a2a2abcdd0cb964
takaya-0808 <takaya1912@gmail.com>
2023-03-25 10:36:03 +0900

    fix

--- todo/src/main/java/com/example/todo/controller/TodoController.java
+++ todo/src/main/java/com/example/todo/controller/TodoController.java
@@ -8,9 +8,15 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/todo/api")
 public class TodoController {
+    
     @RequestMapping("/")
     String home() {
         return "Hello World";
+    }
+
+    @GetMapping(value="/task/{todoId}")
+    public String deleteTask(@PathVariable(value="todoId") Long id) {
+        return "Task Delete ID" + String.valueOf(id);
     }
 
 }
