edf224556e007eec2f84e7ecb1a1ebf765bbf065
takaya-0808 <takaya1912@gmail.com>
2023-04-11 08:01:12 +0900

    fix JdbcTemplate.query =>  jdbcTemplate.query

--- todo/src/main/java/com/example/todo/repository/TodoRepositoryImpl.java
+++ todo/src/main/java/com/example/todo/repository/TodoRepositoryImpl.java
@@ -1,9 +1,12 @@
 package com.example.todo.repository;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+import org.springframework.jdbc.core.RowMapper;
 
 import com.example.todo.model.Todo;
 
@@ -19,5 +22,10 @@ public class TodoRepositoryImpl implements TodoRepository {
         return jdbcTemplate.queryForObject("select * from todo where id=?", new BeanPropertyRowMapper<Todo>(Todo.class), id);
     }
 
+    @Override
+    public List<Todo> findAll() {
+        RowMapper<Todo> rowMapper = new BeanPropertyRowMapper<Todo>(Todo.class);
+        return jdbcTemplate.query("select * from todo", rowMapper);
+    }
 
 }
