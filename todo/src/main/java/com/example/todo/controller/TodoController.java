ec759e98101d3c8707200398af7f2cb4058e71cf
takaya-0808 <takaya1912@gmail.com>
2023-04-12 23:04:22 +0900

    add task

--- todo/src/main/java/com/example/todo/controller/TodoController.java
+++ todo/src/main/java/com/example/todo/controller/TodoController.java
@@ -21,16 +21,23 @@ public class TodoController {
     }
 
     @GetMapping("/task")
-    public List<Todo> GetAllTask() {
+    public List<Todo> getAllTask() {
         return todoRepository.findAll();
+    }
+
+    // todoタスクを追加
+    @PostMapping("/task")
+    public int createTask(@RequestBody Todo todo) {
+        return todoRepository.saveTask(todo);
     }
 
     // 全てのタスクを取得する
     @GetMapping("/task/{id}")
-    public Todo GetOneTask(@PathVariable int id) {
+    public Todo getOneTask(@PathVariable int id) {
         return todoRepository.findById(id);
     }
 
+    // 特定のタスクを削除
     @DeleteMapping("/task/{id}")
     public String deleteTask(@PathVariable Long id) {
         return "Task Delete ID" + String.valueOf(id);
